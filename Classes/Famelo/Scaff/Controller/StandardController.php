260b9a3033771f8c6a29088179fb6d953f16ed32
Marc Neuhaus <mneuhaus@famelo.com>
2012-11-09 11:44:37 +0100

    [WIP] Added current ModuleConfiguration to View Context and updated bootstrap

--- Classes/Famelo/Scaff/Controller/StandardController.php
+++ Classes/Famelo/Scaff/Controller/StandardController.php
@@ -14,11 +14,11 @@ use TYPO3\Flow\Annotations as Flow;
  * @Flow\Scope("singleton")
  */
 class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
-    /**
-     * @var \TYPO3\Flow\Configuration\ConfigurationManager
-     * @Flow\Inject
-     */
-    protected $configurationManager;
+	/**
+	 * @var \TYPO3\Flow\Configuration\ConfigurationManager
+	 * @Flow\Inject
+	 */
+	protected $configurationManager;
 
 	/**
 	 * Index action
@@ -26,11 +26,14 @@ class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	 * @return void
 	 */
 	public function indexAction() {
-		#$scaffRuntime = new \Famelo\Scaff\Core\ScaffRuntime($this->request, $this->response);
-		#$this->view->assign('content', $scaffRuntime->execute());
-
-		if ($this->request->hasArgument("module")) {
-			$this->view->assign("currentModule", $this->request->getArgument("module"));
+		if ($this->request->hasArgument('module')) {
+			$this->view->assign('currentModule', $this->request->getArgument('module'));
+			$this->view->assign('currentModuleConfiguration',
+				$this->configurationManager->getConfiguration(
+					\TYPO3\Flow\Configuration\ConfigurationManager::CONFIGURATION_TYPE_SETTINGS,
+					'Famelo.Scaff.modules.' . $this->request->getArgument('module')
+				)
+			);
 		}
 	}
 
